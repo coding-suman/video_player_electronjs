8b78e129e27bf1ec4d10e6456b1825d6fb2e6a42
SUMAN MANDAL <iamsumandal@gmail.com>
2024-08-19 17:58:19 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false, // Remove the window frame and title bar
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.webContents.openDevTools();
+
+    // Fullscreen and ESC key event handling
+    ipcMain.on('toggle-fullscreen', () => {
+        if (mainWindow.isFullScreen()) {
+            mainWindow.setFullScreen(false);
+        } else {
+            mainWindow.setFullScreen(true);
+        }
+    });
+
+    ipcMain.on('minimize-window', () => {
+        mainWindow.minimize();
+    });
+
+    ipcMain.on('maximize-window', () => {
+        if (mainWindow.isMaximized()) {
+            mainWindow.unmaximize();
+        } else {
+            mainWindow.maximize();
+        }
+    });
+
+    ipcMain.on('toggle-maximize', () => {
+        if (mainWindow.isMaximized()) {
+            mainWindow.unmaximize();
+        } else {
+            mainWindow.maximize();
+        }
+    });
+
+    ipcMain.on('close-window', () => {
+        mainWindow.close();
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
